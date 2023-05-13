cbf81e96f019202af2e42f07c03afdb9203b6d0d
Nikhil <nikhilkumar190804@gmail.com>
2023-05-13 23:24:25 +0530

    Added code for error handling of 7 types

--- Assembler.py
+++ Assembler.py
@@ -1,61 +1,7 @@
-'''def find_type_of_instruction(str):          # func to find the type of instruction 
-    instruction=str.split()[0]
-    if(instruction in possible_instructuion):
-        return possible_instructuion[instruction]
-    else:
-        return -1
-    
-possible_instructuion={              #dict for type of instruction
-   "add": "A",
-   "sub": "A",
-   "mov": "B&C",
-   "ld": "D",
-   "st": "D",
-   "mul": "A",
-   "div": "C",
-   "rs": "B",
-   "ls": "B",
-   "xor": "A",
-   "and": "A",
-   "or": "A",
-   "not": "C",
-   "cmp": "C",
-   "jmp": "E",
-   "jlt": "E",
-   "jgt": "E",
-   "hlt": "F"
-}
-file_reader=[]          # a list to store all the lines as a string of input.txt
-file_handle=open("input.txt","r")
-file_reader=file_handle.readlines()
-line_counter=1
-for i in file_reader:
-    if(len(i)==1): # the line is empty
-        pass
-    else: # line may be valid
-        clean_string=i.strip()
-        result=find_type_of_instruction(clean_string)
-        if(result!=-1):
-            if(result=="A"):
-                #mohitkocall
-                print('a type')
-            elif result=='B' or result=='C' or result=='B&C':
-                #pinkykocall 
-                print("b&c")
-            elif result=='D' or result=='E' or result=='F':
-                #nikkocall
-                print("d&e&f")
-        else:
-            print(f"error has occurred in line: {line_counter} !!!\nInstruction code is wrong ".title())
-            exit(1)
-    line_counter+=1'''
-
-
-
 # registers binary code
 
 d={"R0":"000",
-   "R1":"001",
+"R1":"001",
 "R2":"010",
 "R3":"011",
 "R4":"100",
@@ -88,6 +34,9 @@ hlt="11010"
 var = {}
 register=["R0","R1","R2","R3","R4","R5","R6","FLAGS"]
 
+# a string of special chars for type b instn.
+special_chars="~`!@#$%^&*()_-=+{|}|:;'\"<>,.?/[]/*"
+
 #
 possible_instructuion={              #dict for type of instruction
    "add": "A",
@@ -111,6 +60,25 @@ possible_instructuion={              #dict for type of instruction
 }
 list_output=[]
 #  
+list_of_errors=[]
+
+
+def error_has_occurred():
+    file_handle=open("output.txt","w")
+    file_handle.write(f"Error Has Occurred In Line:{line_counter+1}\n")
+    file_handle.write(list_of_errors[0])
+    file_handle.close()
+    print(list_of_errors)
+    exit(0)
+
+
+def check_for_valid_registers(string):
+    if(string in register):
+        return 1
+    else:
+        return -1
+    
+
 def type_A(instruct,list_output):
     s=""
     if instruct[0]=="add":
@@ -126,14 +94,30 @@ def type_A(instruct,list_output):
     elif instruct[0]=="and":
         s += And 
     s += "00"
-    s += d[instruct[1]]
-    s += d[instruct[2]]
-    s += d[instruct[3]]
+    value1=check_for_valid_registers(instruct[1])
+    value2=check_for_valid_registers(instruct[2])
+    value3=check_for_valid_registers(instruct[3])
+    if(value1==-1 or value2==-1 or value3==-1):#register is wrong
+        error_string="register is invalid".title()
+        list_of_errors.append(error_string)
+        error_has_occurred()
+    else:
+        s += d[instruct[1]]
+        s += d[instruct[2]]
+        s += d[instruct[3]]
     list_output.append(s)
 
 
-
-    
+def check_for_valid_immediate_value(string):
+    symbol=string[0]
+    int_value=string[1:]
+    if(symbol=="$"):
+        if(0<=int(int_value)<=127):
+            return 1
+        else:
+            return -2
+    else:
+        return -1
 
 def type_B(instruct,list_output):
     s=""
@@ -144,12 +128,26 @@ def type_B(instruct,list_output):
     elif instruct[0]=="rs":
         s += rsi
     s += "0"
-    s += d[instruct[1]] 
-    string=instruct[2]
-    number = int(''.join(filter(str.isdigit, string)))
-    fg = format(number, '07b')
-    s += fg
-    list_output.append(s)
+    value1=check_for_valid_registers(instruct[1])
+    if(value1==-1): #register is worng
+        error_string="register is invalid".title()
+        list_of_errors.append(error_string)
+        error_has_occurred()
+    else:
+        s += d[instruct[1]]
+    result=check_for_valid_immediate_value(instruct[2])
+    if(result==-1):
+        list_of_errors.append("syntax error: invalid symbol!!!".title())
+        error_has_occurred()
+    elif (result==-2):
+        list_of_errors.append("invalid immediate value!!!".title())
+        error_has_occurred()
+    else:
+        string=instruct[2]
+        number = int(''.join(filter(str.isdigit, string)))
+        fg = format(number, '07b')
+        s += fg
+        list_output.append(s)
 
 
 def type_C(instruct,list_output):
@@ -164,24 +162,41 @@ def type_C(instruct,list_output):
         s += com
     
     s += "00000"
-    s += d[instruct[1]]
-    s += d[instruct[2]]
-
-    instruct.append(s)
+    value1=check_for_valid_registers(instruct[1])
+    value2=check_for_valid_registers(instruct[2])
+    if(value1==-1 or value2==-1):
+        error_string="register is invalid".title()
+        list_of_errors.append(error_string)
+        error_has_occurred()
+    else:
+        s += d[instruct[1]]
+        s += d[instruct[2]]
+    list_output.append(s)
 
 def type_D(instruct,list_output):
     s=""
-    if instruct[0]=="ld":
-        s += ld 
-        s += "0" #unused bit
-        s += d[instruct[1]] #register
-        s += var[instruct[2]]
+    value1=check_for_valid_registers(instruct[1])
+    if(value1==-1):
+        error_string="register is invalid".title()
+        list_of_errors.append(error_string)
+        error_has_occurred()
     else:
-        s += st
-        s += "0" #unused bit
-        s += d[instruct[1]] #register
-        s += (var[instruct[2]])
-    list_output.append(s)
+        try:
+            if instruct[0]=="ld":
+                s += ld 
+                s += "0" #unused bit
+                s += d[instruct[1]] #register
+                s += var[instruct[2]]
+            else:
+                s += st
+                s += "0" #unused bit
+                s += d[instruct[1]] #register
+                s += (var[instruct[2]])
+                list_output.append(s)
+        except:
+            list_of_errors.append("variable is not declared!!!".title())
+            error_has_occurred()
+
 
 def type_E(instruct,line_counter,list_output):
     s=""
@@ -222,14 +237,6 @@ def file_output(list_output):
         file_handle.flush()
     file_handle.close()
 
-def error_has_occurred(string):
-    file_handle=open("output.txt","w")
-    file_handle.write(string+"\n")
-    file_handle.close()
-
-
-
-
 # main
 with open("input.txt", "r") as file:
     instructions = file.readlines()
@@ -244,17 +251,23 @@ for j in instructions:
         count +=1
     else:
         pass
+print(count)
+print(var)
 
 
 line_counter=0
 is_error=False
+is_variable=True
 for instruction in instructions:
-    if(len(instruction)<=1):
+    if(len(instruction)<=1 or len(instruction.strip())<=1):
         pass
     else:
         instruct = instruction.strip().split()
+        print(instruct)
         result=find_type_of_instruction(instruct[0])
         temp_s=""
+        if instruct[0]!="var":
+            is_variable=False
         if ((instruct[-1])=="hlt"):
             temp_s += hlt
             temp_s += ("0"*11)
@@ -267,42 +280,37 @@ for instruction in instructions:
                 binary = format(count, '07b')
                 var[instruct[1]]=binary
                 count +=1
+            if (is_variable==False):
+                # var is defined later 
+                list_of_errors.append("variable not declared in the beginning!!!".title())
+                error_has_occurred()
         else:
             temp=str(instruct[-1])
             if result !=-1:
                 if(result=="A"):
                     type_A(instruct,list_output)
-                elif result=='B' and temp[0]=="$":
+                elif result=='B' and temp[0] in special_chars:
                     type_B(instruct,list_output)
-                elif result=='C' and temp[0]!="$":
+                elif result=='C' and temp[0] not in special_chars:
                     type_C(instruct,list_output)
                 elif result=='B&C':
-                    if temp[0]=="$":
+                    if temp[0] in special_chars:
                         type_B(instruct,list_output)
                     else:
                         type_C(instruct,list_output)
-
                 elif result=='D':
                     type_D(instruct,list_output)
                 elif result=='E':
                     type_E(instruct,line_counter,list_output)
             else:
-                is_error=True
-                temp_s=""
-                temp_s += "error has occurred in line: ".title()
-                temp_s += str(line_counter+1)
-                temp_s+="\n"
-                temp_s+="wrong instruction!!!".title()
+                # instrution not found
+                list_of_errors.append("wrong instruction!!!".title())
+                error_has_occurred()
                 break
     line_counter+=1
 
 '''
-print(list_output)
 output function here 
+everthing has worked correctly
 '''
-print(var)
-if(is_error==True):
-    error_has_occurred(temp_s)
-else:
-    file_output(list_output)   #output function here
-
+file_output(list_output)   #output function here
