31901ef8201400792750cb943722599d8cb1bbc0
pankaj kumar <pankaj22346@iiitd.ac.in>
2023-05-14 09:22:33 +0530

    edited type b

--- pankajj.py
+++ pankajj.py
@@ -12,22 +12,22 @@ def type_b(command):
         op_b = opcode_b.get(opcode)
             
     else:
-        raise ValueError('Unsupported opcode: {}'.format(opcode)) #error if incoreeect opcode'''
+        raise ValueError('Unsupported opcode: {}'.format(opcode)) #error if incoreeect opcode for finding error'''
     
     if operands in reg.keys() :
         operands_bin = reg.get(operands) #getting resister code
     else:
-        raise ValueError('incorrect registor: {}'.format(operands)) #error if incoreeect register number'''
+        raise ValueError('incorrect registor: {}'.format(operands)) #error if incoreeect register number for finding error'''
     if numbers[0]=="$":
         s1=numbers.replace("$","") #removing dollar symbol
         
     else:
-        raise ValueError('error,it should be $') #error if incoreeect symbol'''
+        raise ValueError('error,it should be $') #error if incoreeect symbol for finding error'''
     if int(s1)<=127 and int(s1)>=0: 
         s2=s1
     else:
-        raise ValueError('error,it should be less than 128') #error if incoreeect decimal input'''
-    return op_b + '_0_'+ operands_bin+ '_' + decimalToBinary(int(s2)) #combining all
+        raise ValueError('error,it should be less than 128') #error if incoreeect decimal input for finding error'''
+    return op_b + '_0_'+ operands_bin+ '_' + decimalToBinary(int(s2)) #combining all for proper output
 assembly_code = 'mov R4 $100'
 print(type_b(assembly_code))
 
