6b923e7ae0470665938a0645b84a115f2b8674d6
MohitMehra2004 <004mohitmehra@gmail.com>
2023-05-15 21:42:29 +0530

    Edited A Type Instructions

--- Mohit.py
+++ Mohit.py
@@ -1,13 +1,17 @@
+# Define the opcode and register mappings as dictionaries
 opcode={"add":"00000","sub":"00001","mul":"00110","xor":"01010","or":"01011","and":"01100"}
 registers={"R0":"000","R1":"001","R2":"010","R3":"011","R4":"100","R5":"101","R6":"110","FLAGS":"111"}
 
+# Define a function to convert assembly code to binary code
 def assemble(instruct):
+
 
     try:
         opcode, operands1,operands2,operands3 = instruct.split(' ')
     except ValueError:
         raise ValueError('Invalid instruction: {}'.format(instruct))
 
+  
     if opcode == 'add':
         opcode_bin = '00000_00'
     elif opcode=='sub':
